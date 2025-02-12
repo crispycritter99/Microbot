893c205308ce92d1c34033699e935252ac79daae
crispycritter99 <drpw27@gmail.com>
2025-02-12 08:29:02 -0800

    adds stuff

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/tickbarb/ThreeTickFishingState.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.microbot.tickbarb;
+public enum ThreeTickFishingState {
+    UseGuam,
+    UseTarAndDrop,
+    ClickFishingSpot,
+    LocatingFishingSpot,
+    Idle
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/tickwc/ThreeTickBarbConfig.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.microbot.tickwc;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("threeTickBarb")
+public interface ThreeTickBarbConfig extends Config {
+}
