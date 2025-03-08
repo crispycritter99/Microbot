a25200412f4f946770e2d92be24c39a7847ba873
pkcs21 <drpw27@gmail.com>
2025-03-08 13:08:53 -0800

    Update AnimatedArmourConfig.java

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/animatedarmour/AnimatedArmourConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/animatedarmour/AnimatedArmourConfig.java
@@ -3,8 +3,20 @@ package net.runelite.client.plugins.microbot.animatedarmour;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 
-@ConfigGroup("example")
+@ConfigGroup("animatedarmour")
 public interface AnimatedArmourConfig extends Config {
+            @ConfigItem(
+            name = "Guide",
+            keyName = "guide",
+            position = 0,
+            description = ""
+
+    )
+    default String guide() {
+        return "Reanimates and kills armour in warriors guild, grabs tokens.\n" +
+                " make sure you have armour (and optionally food) in inventory.\n" +
+                "turn on auto retaliate and ground items runelite plugin";
+    }
 /*    @ConfigItem(
             keyName = "Ore",
             name = "Ore",
