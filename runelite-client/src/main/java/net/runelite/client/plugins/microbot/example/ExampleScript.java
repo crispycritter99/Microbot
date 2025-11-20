3f5c6ce34b4371674066c431327b00453b416a9c
pkcs21 <drpw27@gmail.com>
2025-11-19 21:30:03 -0800

    yeah

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -0,0 +1,100 @@
+package net.runelite.client.plugins.microbot.example;
+
+import net.runelite.api.*;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.widgets.ComponentID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.breakhandler.BreakHandlerScript;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
+import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
+import net.runelite.client.plugins.microbot.util.bank.enums.BankLocation;
+import net.runelite.client.plugins.microbot.util.combat.Rs2Combat;
+import net.runelite.client.plugins.microbot.util.dialogues.Rs2Dialogue;
+import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
+import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2ItemModel;
+import net.runelite.client.plugins.microbot.util.item.Rs2ItemManager;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Magic;
+import net.runelite.client.plugins.microbot.util.magic.Rs2Spells;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
+import net.runelite.client.plugins.microbot.util.menu.NewMenuEntry;
+import net.runelite.client.plugins.microbot.util.npc.Rs2NpcModel;
+import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.prayer.Rs2Prayer;
+import net.runelite.client.plugins.microbot.util.prayer.Rs2PrayerEnum;
+import net.runelite.client.plugins.microbot.util.tile.Rs2Tile;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
+import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
+import net.runelite.client.plugins.skillcalculator.skills.MagicAction;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static net.runelite.client.plugins.microbot.util.Global.sleepUntil;
+
+
+public class ExampleScript extends Script {
+    public static boolean tentacle = false;
+    NPC vorkath;
+    public static boolean lootnet = false;
+     boolean test = false;
+    public boolean run(ExampleConfig config) {
+        Microbot.enableAutoRunOn = false;
+
+        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            try {
+                if (!Microbot.isLoggedIn()) return;
+                if (!super.run()) return;
+                long startTime = System.currentTimeMillis();
+//                    if ((Rs2Player.isAnimating())||Rs2Player.isMoving()) {
+//
+//                        return;
+//
+//                }
+                Rs2Npc.interact(4742,"attack");
+                sleep(400,500);
+                Rs2Walker.walkFastCanvas(new WorldPoint(3371,9556,0));
+                sleepUntilTick(9);
+//                Rs2Walker.walkFastCanvas(new WorldPoint(2800,9568,3));
+//                Rs2Player.waitForXpDrop(Skill.AGILITY,3000);
+//                sleepGaussian(400,200);
+//                Rs2Player.eatAt(40);
+//                    if (Rs2Inventory.get(20885).getQuantity()>27){
+//                        Rs2Inventory.drop(20885);
+//                    }
+//
+////                Rs2GameObject.interact("Chest","Open");
+//
+//                    Rs2GameObject.interact(29742,"Open");
+//                    Rs2Player.waitForXpDrop(Skill.THIEVING,7000);
+//                    sleep(400);
+
+//                Rs2Player.waitForAnimation(1000);
+//                shutdown();
+                long endTime = System.currentTimeMillis();
+                long totalTime = endTime - startTime;
+                System.out.println("Total time for loop " + totalTime);
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }, 0, 600, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+    }
+
+}
